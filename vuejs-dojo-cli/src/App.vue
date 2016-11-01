b02697a7acacb0b6cddab374b6596dbb01c211b4
Alan <alan.pereira@rgsistemas.com.br>
2016-11-01 18:56:13 -0200

    Dojo vue-cli

---
+++ vuejs-dojo-cli/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <form-favorito @favoritoinformado="cadastrarFavorito"></form-favorito>
+    <lista-favorito></lista-favorito>
+  </div>
+</template>
+
+<script>
+import FormFavorito from './components/FormFavorito'
+import ListaFavorito from './components/ListaFavorito'
+
+export default {
+  name: 'app',
+  data: function () {
+    return {
+      favoritos: []
+    }
+  },
+  components: {
+    FormFavorito,
+    ListaFavorito
+  },
+  methods: {
+    cadastrarFavorito: function (campos) {
+      console.log('Adicionada')
+      this.favoritos.push(campos)
+    }
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vuejs-dojo-cli/src/components/FormFavorito.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+      <h1>Olá Mundo</h1>
+      <form class="form-horizontal">
+        <div class="form-group">
+              <label>Pagina</label>
+              <input type="text" class="form-control" v-model="campos.pagina" placeholder="Nome Da Pagina" autofocus="">
+          </div>
+          <div class="form-group">
+              <label>URL</label>
+              <input type="url" v-model="campos.url" class="form-control" placeholder="Url da Pagina" >
+          </div>
+          <button type="submit" class="btn btn-default" v-if="validarFormulario" @click.prevent = "obterFavoritoInformado">Cadastrar Favorito</button>
+        </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'formulariofavorito',
+
+  data: function () {
+    return {
+      campos: {
+        pagina: '',
+        url: ''
+      }
+    }
+  },
+
+  methods: {
+    obterFavoritoInformado: function () {
+      this.$emit('favoritoinformado', this.campos)
+    }
+  },
+
+  computed: {
+    validarFormulario: function () {
+      return this.campos.pagina !== '' && this.campos.url !== ''
+    }
+  }
+
+}
+</script>
+
