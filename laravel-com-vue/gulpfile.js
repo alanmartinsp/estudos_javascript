05b6da55490b209c6b0b1d5ef02518213bb25279
Alan <alan.pereira@rgsistemas.com.br>
2016-11-08 13:21:59 -0200

    webpack, gulp, elixir

--- laravel-com-vue/gulpfile.js
+++ laravel-com-vue/gulpfile.js
@@ -17,6 +17,30 @@ elixir(mix => {
 
 	mix.webpack('app.js');
 
+	// mix.browserify('app.js');
+
+	// mix.rollup('app.js');
+
     mix.sass('app.scss');
+
+    //Buscamos na raiz do projeto o css desejado
+    mix.styles([
+    	'./node_modules/font-awesome/css/font-awesome.css'
+    ], 'public/css/vendor.css');
+
+    //Copiamos os arquivos e informamos qual será o diretorio do mesmo
+    mix.copy('node_modules/font-awesome/fonts', 'public/build/fonts');
+
+    //Versionamos os css para que o brownser tenha sempre que atualizar os arquivos
+    mix.version([
+    	'public/css/vendor.css',
+    	'public/css/app.css',
+    	'public/js/app.js'
+    ]);
+
+    //Atualiza página automaticamente
+    mix.browserSync({
+    	proxy: 'localhost:8000'
+    });
 
 });
--- laravel-com-vue/resources/views/welcome.blade.php
+++ laravel-com-vue/resources/views/welcome.blade.php
@@ -2,13 +2,15 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{ elixir('css/app.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{ elixir('css/vendor.css')}}">
         <title>Laravel</title>
 
     </head>
     <body>
         <div class="container">
-            <h1>Curso Laravel e Vue.js</h1>
+            <h1>  <i class = "fa fa-fw fa-github"></i>  Curso Laravel e Vue.js</h1>
         </div>
     </body>
 </html>
+
