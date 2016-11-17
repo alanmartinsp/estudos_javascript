4a1a70f0c1c687b9e60289d266e9f394226bcb54
Alan <alan.pereira@rgsistemas.com.br>
2016-11-17 13:22:36 -0200

    criando controller

---
+++ alura-ES6/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,15 @@
+class NegociacaoController {
+
+    adiciona(event) {
+
+        event.preventDefault();
+
+        let $ = document.querySelector.bind(document);
+
+        let inputData = $('#data');
+        let inputQuantidade = $('#quantidade');
+        let inputValor = $('#valor');
+
+    }
+
+}
