9a989d590a75dd615b4243f14a5451b503f231df
Alan <alan@rgsistemas.com>
2016-11-18 13:24:10 -0200

    Criando método adiciona

--- alura-ES6/client/js/app/controllers/NegociacaoController.js
+++ alura-ES6/client/js/app/controllers/NegociacaoController.js
@@ -1,14 +1,52 @@
 class NegociacaoController {
 
+    constructor() {
+        
+        let $ = document.querySelector.bind(document);
+
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+
+    }
+
     adiciona(event) {
 
         event.preventDefault();
 
-        let $ = document.querySelector.bind(document);
+        //Convertendo data
+        // let data = new Date(this._inputData.value.split("-"));
+        // let data = new Date(this._inputData.value.replace(/-/g, ','));
+        
+        // ----------------------------------------------------
+        // let data = new Date(...
+        //     this._inputData.value
+        //         .split("-")
+        //         .map(function(item, indece){
+        //             if( indece === 1) {
+        //                 return item - 1;
+        //             }
 
-        let inputData = $('#data');
-        let inputQuantidade = $('#quantidade');
-        let inputValor = $('#valor');
+        //             return item;
+        //         })
+        // );
+
+        // ----------------------------------------------------
+        // Spread operator (...)
+        // Três pontinhos indica o uso do spread operator e com ele o JavaScript extrairá cada elemento do array.
+        let data = new Date(...
+            this._inputData.value
+                .split("-")
+                //PODEMOS DEIXAR O CÓDIGO MENOS VERBOSO COM ARROW FUNCTIONS 
+                //COM ARROW FUNCTION NÃO PRECISAMOS ESCREVER A PALAVRA FUNCTION.
+                //QUANDO ELA POSSUI APENAS UMA LINHA, NÃO PRECISAMOS USAR O BLOCO E NEM A PALAVRA CHAVE return
+                .map((item, indece) => indece === 1 ? item - 1 : item));
+
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade,
+            this._inputValor
+        );
 
     }
 
